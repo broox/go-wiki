a0f9490b58d9f77e487352feb73fb5b0ca9e7fe6
Derek Brooks <derek@broox.com>
2013-04-18 13:02:05 -0500

    build simple page edit form

--- wiki.go
+++ wiki.go
@@ -43,7 +43,20 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[lenPath:]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{ Title: title }
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>" +
+		       "<form action=\"/save/%s\" method=\"POST\">" +
+		       "<textarea name=\"body\">%s</textarea><br>" +
+		       "<input type=\"submit\" value=\"Save\">" +
+		       "</form>", p.Title, p.Title, p.Body)
+}
 func main() {
         http.HandleFunc(pathPrefix, viewHandler)
+        http.HandleFunc("/edit/", editHandler)
         http.ListenAndServe(":8080", nil)
 }
